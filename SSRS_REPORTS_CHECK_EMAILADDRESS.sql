b1f47d44d4d226222678e29174dbb56620224c71
Umar Khan <umarkhan252@gmail.com>
2019-09-16 12:17:27 -0700

    SSRS subscription check stored procedure

---
+++ SSRS_REPORTS_CHECK_EMAILADDRESS.sql
@@ -0,0 +1,64 @@
+/*
+	This query is a modification of the query on this link
+	https://social.msdn.microsoft.com/Forums/sqlserver/en-US/002475a9-0556-4722-af38-d17ab6972cbe/details-of-reports-scheduled-on-ssrs-report-name-userdetails-frequency-etc?forum=sqlreportingservices
+*/
+
+CREATE PROCEDURE ReportSubscriptionCheck(
+										@EmailAddress NVARCHAR (MAX)
+										)
+AS
+	BEGIN
+		SELECT
+			c.Name								   AS ReportName,
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="TO"])[1]',
+													 'nvarchar(max)'
+												 ) AS [To],
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="CC"])[1]',
+													 'nvarchar(max)'
+												 ) AS CC,
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="BCC"])[1]',
+													 'nvarchar(max)'
+												 ) AS BCC,
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="RenderFormat"])[1]',
+													 'nvarchar(max)'
+												 ) AS [Render Format],
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="Subject"])[1]',
+													 'nvarchar(max)'
+												 ) AS Subject,
+			LastStatus,
+			EventType,
+			LastRunTime,
+			DeliveryExtension,
+			Version
+		FROM
+			dbo.Catalog					  AS c
+			INNER JOIN dbo.Subscriptions  AS S ON c.ItemID = S.Report_OID
+			INNER JOIN dbo.ReportSchedule AS R ON S.SubscriptionID = R.SubscriptionID
+		WHERE
+			CONVERT(XML, ExtensionSettings).value(
+													 '(//ParameterValue/Value[../Name="TO"])[1]',
+													 'nvarchar(max)'
+												 ) LIKE '%' + @EmailAddress
+														+ '%'
+			OR CONVERT(XML, ExtensionSettings).value(
+														'(//ParameterValue/Value[../Name="CC"])[1]',
+														'nvarchar(max)'
+													) LIKE '%'
+														   + @EmailAddress
+														   + '%'
+			OR CONVERT(XML, ExtensionSettings).value(
+														'(//ParameterValue/Value[../Name="BCC"])[1]',
+														'nvarchar(max)'
+													) LIKE '%'
+														   + @EmailAddress
+														   + '%';
+
+	END;
+
+	--EXEC dbo.ReportSubscriptionCheck @EmailAddress = N'' -- nvarchar(max)
+	
